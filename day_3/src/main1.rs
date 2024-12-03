a12650450e8c99a5a2905e221a4feeb2650ae1f9
MajorX234 <majorx234@googlemail.com>
2024-12-04 00:42:45 +0100

    Add solution day3 task2

--- day_3/src/main1.rs
+++ day_3/src/main1.rs
@@ -15,12 +15,25 @@ fn main() {
 
     let raw_input: String = read_to_string(file_name).expect("could not read file");
 
-    let mut sum = 0;
+    let mut sum_task1 = 0;
+    let mut sum_task2 = 0;
     let complete_size = raw_input.len();
     let re = Regex::new(r"^mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let re_dont = Regex::new(r"^don't\(\)").unwrap();
+    let re_do = Regex::new(r"^do\(\)").unwrap();
 
+    let mut state = true;
     for i in 0..(complete_size - 12) {
         let window: &str = &raw_input[i..i + 12];
+        if state {
+            if re_dont.is_match(window) {
+                state = false;
+            }
+        } else {
+            if re_do.is_match(window) {
+                state = true;
+            }
+        }
         if re.is_match(window) {
             let Some((_, [value1_raw, value2_raw])) =
                 re.captures(window).map(|caps| caps.extract())
@@ -29,8 +42,11 @@ fn main() {
             };
             let value1 = value1_raw.parse::<u32>().unwrap();
             let value2 = value2_raw.parse::<u32>().unwrap();
-            sum += value1 * value2;
+            sum_task1 += value1 * value2;
+            if state {
+                sum_task2 += value1 * value2;
+            }
         }
     }
-    println!("sum: {}", sum);
+    println!("sum_task1: {} sum_task2: {}", sum_task1, sum_task2);
 }
