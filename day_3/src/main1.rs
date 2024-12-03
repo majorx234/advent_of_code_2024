e8a51b052d6f96973422ec771454fb7317eb6d04
MajorX234 <majorx234@googlemail.com>
2024-12-04 00:25:22 +0100

    Add solution day3 task1

--- day_3/src/main1.rs
+++ day_3/src/main1.rs
@@ -15,13 +15,22 @@ fn main() {
 
     let raw_input: String = read_to_string(file_name).expect("could not read file");
 
+    let mut sum = 0;
     let complete_size = raw_input.len();
+    let re = Regex::new(r"^mul\(([0-9]+),([0-9]+)\)").unwrap();
+
     for i in 0..(complete_size - 12) {
         let window: &str = &raw_input[i..i + 12];
-        let re = Regex::new(r"^mul\([0-9]+,[0-9]+\)").unwrap();
         if re.is_match(window) {
-            let re_no1 = Regex::new(r"\([0-9]+,");
-            let re_no2 = Regex::new(r",[0-9]+\)");
+            let Some((_, [value1_raw, value2_raw])) =
+                re.captures(window).map(|caps| caps.extract())
+            else {
+                return;
+            };
+            let value1 = value1_raw.parse::<u32>().unwrap();
+            let value2 = value2_raw.parse::<u32>().unwrap();
+            sum += value1 * value2;
         }
     }
+    println!("sum: {}", sum);
 }
